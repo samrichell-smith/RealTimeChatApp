e9238869147ca1f4362f198d771588a4c61fd9dc
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-02 20:58:46 +1300

    create user authentication state management

---
+++ frontEnd/src/store/useAuthStore.js
@@ -0,0 +1,11 @@
+import {create} from "zustand"
+
+
+export const useAuthStore = create((set) => ({
+    authUser:null,
+    isSigningUp:false,
+    isLoggingIn:false,
+    isUpdatingProfile:false,
+
+    isCheckingAuth:true,
+}))
