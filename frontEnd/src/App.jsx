14b4da60413e51a333f4e3f2a81345a1d4615d1d
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-02 19:42:24 +1300

    Created Page Components

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
-    <div className='text-red-500'>Hello World</div>
+    <div>
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+
+    </div>
   )
 }
 
