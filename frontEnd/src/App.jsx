57c7860e4b4b4819c5edde03e76f2a27f2e88b82
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-04 21:05:36 +1300

    Add checking auth loader

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -7,15 +7,22 @@ import LoginPage from "./pages/LoginPage"
 import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import { useAuthStore } from './store/useAuthStore'
+import {Loader} from "lucide-react"
 
 const App = () => {
-  const {authUser,checkAuth}= useAuthStore()
+  const {authUser,checkAuth, isCheckingAuth }= useAuthStore()
 
   useEffect(() => {
     checkAuth()
   }, [checkAuth]);
 
   console.log({authUser})
+
+  if(isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
   
   return (
     <div>
