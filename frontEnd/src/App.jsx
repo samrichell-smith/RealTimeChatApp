284120e09fe61247c8f8be36cb3a697bc30ff574
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-08 19:11:17 +1300

    add sign up page css

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -30,11 +30,11 @@ const App = () => {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
 
     </div>
--- frontEnd/src/pages/SignUpPage.jsx
+++ frontEnd/src/pages/SignUpPage.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore';
 
 const SignUpPage = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+
   return (
-    <div>SignUpPage</div>
+    <div className='min-h-screen grid lg:grid-cols-2'>
+      {/* left side of form */}
+      <div className='flex flex-col justify-center items-center p-6 sm:p-12'>
+        <div className='w-full max-w-md space-y-8'>
+          <div className='text-center mb-8'>
+            <div className='flex flex-col items-center gap-2 group'>
+              <div className ='size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors'>
+                <MessageSSquare className='size-6 text-primary' />
+              </div>
+              <h1 className='text-2xl font-bold mt-2'>Create Account</h1>
+              <p className='text-base-content/60' >Get started with a free account</p>
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
