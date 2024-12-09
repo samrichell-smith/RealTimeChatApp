3b1e0cba8cd9533d7d8dd07ca5444a01e63360b8
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-09 22:20:27 +1300

    Fixed Routing Bug Causing a CORS error

--- frontEnd/src/pages/SignUpPage.jsx
+++ frontEnd/src/pages/SignUpPage.jsx
@@ -34,6 +34,10 @@ const SignUpPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const success = validateForm();
+
+    if (success === true) signup(formData);
   };
 
   return (
--- frontEnd/src/store/useAuthStore.js
+++ frontEnd/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../components/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -22,5 +23,18 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  signup: async (data) => {},
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+        const res = await axiosInstance.post("/auth/signup", data);
+        set({ authUser: res.data });
+        toast.success("Account created successfully");
+        
+        
+    } catch (error) {
+        toast.error(error.response.data.message);
+    } finally {
+        set({ isSigningUp: false });
+    }
+  },
 }));
