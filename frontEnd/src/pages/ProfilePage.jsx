0713c5c963f7b8e0a42713408b4badd44c37efd6
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-12 16:54:01 +1300

    Add profile page html

--- frontEnd/src/pages/ProfilePage.jsx
+++ frontEnd/src/pages/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAuthStore } from '../store/useAuthStore'
+import { Camera} from "lucide-react"
 
 
 const ProfilePage = () => {
@@ -28,7 +29,22 @@ const ProfilePage = () => {
                 alt="Profile"
                 className='size-32 rounded-full object-cover border-4'
               />
-
+              <label 
+                htmlFor='avatar-upload'
+                className={`
+                  absolute bottom-0 right-0 bg-base-content hover:scale-105 p-2 rounded-full cursor-pointer transition-all duration-200 ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
+                `}
+              >
+                <Camera className='w-5 h-5 text-base-200'/>
+                <input 
+                  type="file"
+                  id="avatar-upload"
+                  className='hidden'
+                  accept='image/*'
+                  onChange={handleImageUpload}
+                  disabled={isUpdatingProfile}
+                />
+              </label>
             </div>
 
           </div>
