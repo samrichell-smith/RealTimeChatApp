a156e5aadd4a2b0fbfaed11be97ead44dc28e216
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-17 17:40:24 +1300

    Added example text explaining skills I've learnt on this project

--- frontEnd/src/pages/SettingsPage.jsx
+++ frontEnd/src/pages/SettingsPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+const PREVIEW_MESSAGES = [
+  { id: 1, content: "This project was one of my first attempts at building a full stack application using react and the MERN stack.", isSent: false },
+  {id: 2, content: "I learnt and refined a lot of skills, such as database integration with MongoDB, WebSocket communication, state management with React hooks, routing and authentication middleware, and version control with Git", isSent: true},
+]
+ 
 const SettingsPage = () => {
   return (
     <div>SettingsPage</div>
