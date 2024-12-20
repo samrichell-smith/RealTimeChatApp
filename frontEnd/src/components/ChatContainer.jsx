26a6b729ef8eef972f044dc20205b490e79b68b6
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-20 18:42:14 +1300

    Create Message Skeleton

--- frontEnd/src/components/ChatContainer.jsx
+++ frontEnd/src/components/ChatContainer.jsx
@@ -10,7 +10,13 @@ const ChatContainer = () => {
     getMessages(selectedUser._id)
   }, [selectedUser._id, getMessages])
 
-  if(isMessagesLoading) return <div>Loading...</div>
+  if(isMessagesLoading) return (
+    <div className='flex-1 flex flex-col overflow-auto'>
+      <ChatHeader />
+      <MessageSkeleton />
+      <MessageInput />
+    </div>
+  )
 
   return (
     <div className='flex-1 flex flex-col overflow-auto'>
