08df331714cad08d1e78d8fb877a2b06eade5f13
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-20 15:27:08 +1300

    Add Sidebar Component

--- frontEnd/src/components/Sidebar.jsx
+++ frontEnd/src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
 import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } =
     useChatStore();
+  
+  const {onlineUsers} = useAuthStore()
 
-  const onlineUsers = [];
+  
 
   useEffect(() => {
     getUsers();
