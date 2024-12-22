f7eb9d79711b4130eec357ba2ec98014ad908375
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-22 16:04:21 +1300

    Add chat image uploading preview

--- frontEnd/src/components/ChatContainer.jsx
+++ frontEnd/src/components/ChatContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import ChatHeader from './ChatHeader'
 import MessageInput from './MessageInput'
+import MessageSkeleton from './skeletons/MessageSkeleton'
 
 const ChatContainer = () => {
   const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore()
@@ -10,13 +11,13 @@ const ChatContainer = () => {
     getMessages(selectedUser._id)
   }, [selectedUser._id, getMessages])
 
-  if(isMessagesLoading) return (
+  if(isMessagesLoading) {return (
     <div className='flex-1 flex flex-col overflow-auto'>
       <ChatHeader />
       <MessageSkeleton />
       <MessageInput />
     </div>
-  )
+  )}
 
   return (
     <div className='flex-1 flex flex-col overflow-auto'>
