2dfaba8d592b97a88ac88ce2764a7c4234284746
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-27 22:46:51 +1300

    Format Middleware and routes

--- backEnd/src/routes/auth.route.js
+++ backEnd/src/routes/auth.route.js
@@ -1,6 +1,12 @@
-import express from "express"
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
-import {protectRoute} from "../middleware/auth.middleware.js"
+import express from "express";
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,8 +14,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.put("/update-profile", protectRoute, updateProfile)
+router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, checkAuth)
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
