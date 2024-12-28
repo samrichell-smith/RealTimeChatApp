ecd4a07c9a6f2b15179ada832981efddeadf2326
Sam Richell-Smith <samrs6012@gmail.com>
2024-12-28 16:37:44 +1300

    Connect frontend and backend servers

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +15,7 @@ dotenv.config();
  
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -25,6 +28,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontEnd/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontEnd", "dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
