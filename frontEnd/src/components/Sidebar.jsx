a4062c750f00af10e99a246916385a8a2b86e2ff
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-01 17:27:14 +1300

    Update todo list

--- frontEnd/src/components/Sidebar.jsx
+++ frontEnd/src/components/Sidebar.jsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
           <Users className="size-6" />
           <span className="font-medium hidden lg:block">Contacts</span>
         </div>
-        {/* todo: online filter toggle */}
+        
         <div className="mt-3 hidden lg:flex items-center gap-2">
           <label className="cursor-pointer flex items-center gap-2">
             <input
